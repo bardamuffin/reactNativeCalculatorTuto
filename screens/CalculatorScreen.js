9f08591e21c2f6f93aad59425a699a8a11c91e2c
commodus <bardamuffin@gmail.com>
2019-07-28 19:48:58 +0200

    library imported

--- screens/CalculatorScreen.js
+++ screens/CalculatorScreen.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { View, Text, } from 'react-native';
 
+require("./../lib/swisscalc.lib.format.js");
+require("./../lib/swisscalc.lib.operator.js");
+require("./../lib/swisscalc.lib.operatorCache.js");
+require("./../lib/swisscalc.lib.shuntingYard.js");
+require("./../lib/swisscalc.calc.calculator.js");
+require("./../lib/swisscalc.display.numericDisplay.js");
+require("./../lib/swisscalc.display.memoryDisplay.js");
+
 export default class CalculatorScreen extends React.Component {
 
     render() {
