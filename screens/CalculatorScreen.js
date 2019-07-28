9b4a52e2b7a3c26da77cc22ce11da2be792f9195
commodus <bardamuffin@gmail.com>
2019-07-28 19:30:24 +0200

    Project initialized

---
+++ screens/CalculatorScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, } from 'react-native';
+
+export default class CalculatorScreen extends React.Component {
+
+    render() {
+        return (
+            <View style={{paddingTop:50}}>
+                <Text>Calculator Screen</Text>
+            </View>
+        )
+    }
+}
